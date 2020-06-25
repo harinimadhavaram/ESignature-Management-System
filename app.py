b23683db8fda0ae66ae86d767fddfa348dd2ed7e
Keerthi-Gudipudi <kgudipudi@csus.edu>
2020-06-24 22:23:03 -0700

    latest changes of forget password

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from dominate.tags import a
 from flask import Flask, render_template, json, request, redirect, url_for, flash,session,abort
 from flask_bootstrap import Bootstrap
 from flask_mysqldb import MySQL
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+UPLOAD_FOLDER = '/Users/keerthigudipudi/esig/esignaturedb/static/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','svg'}
+
 
 from markupsafe import escape
 import smtplib
@@ -29,10 +34,47 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '12345678'
 app.config['MYSQL_DB'] = 'calpers_users'
 app.secret_key='hello'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 5 * 1024*1024
 ts = itsdangerous.URLSafeTimedSerializer(app.config["SECRET_KEY"])
 mysql = MySQL(app)
 
 Bootstrap(app)
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    cursor1 = mysql.connection.cursor()
+    file_path = ''
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_path = UPLOAD_FOLDER+"/"+filename
+            print("-"*30)
+            print(file_path)
+            cursor1.execute("insert into Image_path (Name,Imagepath) values(%s,%s)",[filename, file_path])
+            mysql.connection.commit()
+            return redirect(url_for('uploaded_file',filename=filename))
+    return render_template('upload_signature.html',user_image = file_path)
+
+@app.route('/uploads/<filename>',methods=['GET', 'POST'])
+def uploaded_file(filename):
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
 
 @app.route('/',methods=['GET','POST'])
 def login():
