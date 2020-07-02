a59d3118b761de73a92bd1cad93844be9ff5958a
Keerthi-Gudipudi <kgudipudi@csus.edu>
2020-07-01 18:44:19 -0700

    6:43 signature

--- app.py
+++ app.py
@@ -1,37 +1,36 @@
 import itsdangerous
-from dominate.tags import a
+#from dominate.tags import a
 from flask import Flask, render_template, json, request, redirect, url_for, flash,session,abort
 from flask_bootstrap import Bootstrap
+import smtplib
+import sys
+import os
 from flask_mysqldb import MySQL
+#from passlib.hash import sha256_crypt
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+#from passlib.handlers.sha2_crypt import sha256_crypt
+
+fromaddr = "phaseteaching@gmail.com"
+msg = MIMEMultipart()
 import numpy as np
-import cv2
+#import cv2
 from matplotlib import pyplot as plt
 
 from werkzeug.utils import secure_filename
 from flask import send_from_directory
-UPLOAD_FOLDER = '/Users/keerthigudipudi/esig/esignaturedb/static/uploads/'
+UPLOAD_FOLDER = '/Users/keerthigudipudi/esig/esignaturedb/ESignature-Management-System/esignature_ui/static/uploads'
 # UPLOAD_FOLDER = 'E:/CSUS/CSC 230/Project/ESignature-Management-System/templates/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg','svg'}
 
 from markupsafe import escape
 import smtplib
-import sys
-import os
-import re
-#from Crypto.Cipher import AES
-#import base64
-from passlib.hash import sha256_crypt
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email import encoders
-fromaddr = "phaseteaching@gmail.com"
-msg = MIMEMultipart()
-#cipher = AES.new("hello",AES.MODE_ECB)
-#BLOCK_SIZE = 32
-#from Werkzeug import generate_password_hash, check_password_hash
 
-app = Flask(__name__)
+
+app = Flask(__name__, static_url_path='/static')
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
@@ -59,9 +58,91 @@ def login():
 def home():
     return render_template('home_page.html') 
 
-@app.route('/profilepage')
+@app.route('/profilepage',methods=['GET','POST'])
 def profile():
-    return render_template('profile_page.html') 
+    cursor1 = mysql.connection.cursor()
+    file_path = session['file_path']
+    name = session['filename']
+    # print(file_path)
+    username = session['user']
+    email=session['email']
+    cursor1.execute("select Name,Imagepath,Preferredname from Image_path where username=%s", [username])
+    # print('1111')
+    images = cursor1.fetchall()
+    # print('1111')
+    # print(images)
+    names = images[0][0].split(",")
+    imagepath = images[0][1].split(",")
+    preferredname=images[0][2]
+    session['names'] = names
+    session['imagepath'] = imagepath
+
+    print(names)
+    print(imagepath)
+    return render_template('profile_page.html',name=names,email=email,imagepath=imagepath,prename=preferredname)
+
+@app.route('/editprofile',methods=['GET','POST'])
+def editprofile():
+
+    #session['update']=None
+
+    cursor1 = mysql.connection.cursor()
+    email=request.form['ta1']
+    firstname=request.form['fta1']
+    lastname = request.form['lta1']
+    orgname=request.form['ota1']
+    username=session['user']
+    #emailid=request.form['ta']
+    #id=request.form['thetext']
+    #print(id)
+    print(email)
+    #print(emailid)
+    currentemail=session['email']
+    if request.method == 'POST':
+        cursor1.execute("update calpers_users.user_details set Firstname = %s, Lastname = %s, Orgname = %s where user_details.Username = %s",[firstname,lastname,orgname,username])
+        mysql.connection.autocommit(True)
+
+
+        #  if session['update']=="TRUE":
+       #     cursor1.execute("select Email_id from calpers_users.user_details where user_details.Username = %s",[username])
+        #    emailid=cursor1.fetchone()[0]
+         #   session['email']=emailid
+          #  print("slect data", session['update'])
+           # cursor1.execute("select defaultsign from calpers_users.Image_path where Image_path.username=%s",[username])
+            #defaultsign=cursor1.fetchone()[0]
+            #session.pop('update',None)
+            #return render_template('profile_page.html',emailid=emailid,defaultsign=defaultsign)
+
+
+        #else:
+        cursor1.execute("update calpers_users.user_details set Email_id = %s where user_details.Email_id = %s",[email,currentemail])
+        mysql.connection.autocommit(True)
+        cursor1.execute("select Email_id,Firstname,Lastname,Orgname from calpers_users.user_details where user_details.Username = %s",[username])
+        result=cursor1.fetchall()
+        email=result[0][0]
+        firstname=result[0][1]
+        lastname=result[0][2]
+        orgname=result[0][3]
+        defaultsign=request.form.getlist('radio')
+        cursor1.execute("update calpers_users.Image_path set defaultsign = %s where username = %s",[defaultsign,username])
+        mysql.connection.autocommit(True)
+        cursor1.execute("select defaultsign from calpers_users.Image_path where username = %s",[username])
+        defaultsign1=cursor1.fetchone()[0]
+        session['firstname'] = firstname
+        session['lastname'] = lastname
+        session['orgname'] = orgname
+        session['email']=email
+
+        session['defaultsign']=defaultsign1
+        print("updating the query", session['update'])
+            #session['update']='TRUE'
+        return redirect(url_for("profile"))
+
+
+    else:
+        print("-"*40)
+        print("error")
+
 
 @app.route('/reset-password')
 def resetpassword():
@@ -85,47 +166,28 @@ def showlogin():
         username = request.form['inputUsername1']
         password= request.form['inputPassword1']
 
-        #password1 = sha256_crypt.hash(password)
 
-        #pwd=sha256_crypt.verify(password,password1)
-        #hash=sha256_crypt.hash("keerthi")
-        ##pwd2=sha256_crypt.verify("keerthi",hash)
-        #print(pwd2)
-        #print(passwordlogin)
         if username and password:
-        #if  'inputUsername1' in request.form and 'inputPassword1' in request.form:
-            #print('0000000')
-            #print('111111')
-            #username = request.form['inputUsername1']
-            #password = request.form['inputPassword1']
 
-            #cursor2.execute("select * from user_details where user_details.Username=%s and user_details.Pwd=%s",[username,pwd])
-            #cursor2.execute("select Pwd from user_details where user_details.Username=%s",[username])
 
-            #cursor2.execute("select * from user_details where user_details.Username=%s and user_details.Pwd=%s",[username,password])
-            #print('22222')
-            #password1 = cursor2.fetchall()
-            exists=cursor2.execute("select Pwd,Email_id from user_details where user_details.Username=%s ",[username])
+
+            cursor2.execute("select * from user_details where user_details.Username=%s and user_details.Pwd=%s",[username,password])
+
+            #exists=cursor2.execute("select Pwd,Email_id from user_details where user_details.Username=%s ",[username])
 
             userdetail=cursor2.fetchall()
             print(userdetail)
-            if exists != 0:
-                match=sha256_crypt.verify(password,userdetail[0][0])
-                print('sha1')
-            else:
-                match=0
-            #pwd=sha256_crypt.verify(passwordlogin,password1)
-            #pwd=check_password_hash(password,userexists)
-            #print('222223')
+            #if exists != 0:
+            #    match=sha256_crypt.verify(password,userdetail[0][0])
+             #   print('sha1')
+            #else:
+             #   match=0
 
-            #pwd=sha256_crypt.verify(request.form['inputPassword1'],userexists)
-            #print(pwd)
-            #password1 = cipher.decrypt(baes64.b64decode(password))
-            #if pwd:
-            if match:
+            if userdetail:
                 session['loggedin']= True
                 session['user']=username
-                session['email']=userdetail[0][1]
+                session['email']=userdetail[0][2]
+                print(userdetail[0][2])
                
                 if 'user' in session:
                     print(session['user'])
@@ -137,7 +199,7 @@ def showlogin():
 
             else:
                 print('Login failed')
-                flash('USERNAME OR PASSWORD INCORRECT')
+                flash('USERNAME OR PASSWORD INCORRECT', 'failure')
                 #msg='Incorrect username/password!'
                 return render_template('index.html')
 
@@ -178,41 +240,37 @@ def showsignup():
         _username = request.form['inputUsername']
         _pwd = request.form['inputPassword']
         _email = request.form['inputEmail']
-       # _prename = request.form['inputPreferredname']
-        #_que1 = str(request.form.get('dropdown1'))
         _que1 = request.form['dropdown']
         _ans1 = request.form['dropdown-answer']
-        #_que2 = request.form['dropdown2']
-        #_ans2 = request.form['security_ans2']
-        #password1 = base64.b64encode(cipher.encrypt(_pwd))
-        password1 = sha256_crypt.hash(_pwd)
-        #password1=generate_password_hash(_pwd)
+
+        #password1 = sha256_crypt.hash(_pwd)
+
 
         print(' two')
         cursor1.execute("select 1 from user_details where user_details.Username=%s",[_username])
-	#cursor1.execute("select * from user_details where user_details.Username='%s'",_name)
         data=cursor1.fetchall()
+        cursor1.execute("select 1 from user_details where user_details.Email_id=%s", [_email])
+        emaildata=cursor1.fetchall()
+
 
         if _username and _pwd and _email and _que1 and _ans1:
             print('three')
-            #cursor.callproc('sp_createUser',(_name, _pwd, _email,_prename, _que1, _ans1, _que2, _ans2))
-            #cursor1.execute("select * from user_details where user_details.Username=%s",_name)
-            #data=cursor1.fetchall(
+
             print('4')
-            if len(data) is 0:
+            if len(data) is 0 and len(emaildata) is 0:
 
                 print('5')
-                cursor1.execute("insert into user_details(Username,Pwd,Email_id,SecQue1,Ans1) values (%s,%s,%s,%s,%s)",[_username,password1,_email,_que1,_ans1])
+                #cursor1.execute("insert into user_details(Username,Pwd,Email_id,SecQue1,Ans1) values (%s,%s,%s,%s,%s)",[_username,password1,_email,_que1,_ans1])
 
-                #cursor1.execute("insert into user_details(Username,Pwd,Email_id,SecQue1,Ans1) values (%s,%s,%s,%s,%s)",[_username,_pwd,_email,_que1,_ans1])
+                cursor1.execute("insert into user_details(Username,Pwd,Email_id,SecQue1,Ans1) values (%s,%s,%s,%s,%s)",[_username,_pwd,_email,_que1,_ans1])
                 mysql.connection.commit()
-                flash("Account created")
+                flash(u'Account created', 'success')
                 print('6')
                 #return render_template('login.html')
                 return redirect(url_for("login"))
 
             else:
-                flash("ACCOUNT ALREADY EXISTS. TRY LOGGIN IN")
+                flash(u'ACCOUNT ALREADY EXISTS. TRY LOGGIN IN', 'failure')
                 print("Username Exists")
 
                 return redirect(url_for("signup"))
@@ -298,9 +356,9 @@ def forgotpassword():
         exists=cursor4.execute("select Username from user_details where user_details.Email_id=%s and user_details.SecQue1=%s and user_details.Ans1=%s", [_email,_que1,_ans1])
         user=cursor4.fetchall()
         if exists is 0:
-            flash("Details are incorrect")
+            flash(u'Details are incorrect', 'failure')
             return redirect(url_for("forgotpwd"))
-    
+
     
     if user is not 0:
         toaddr = _email
@@ -315,7 +373,7 @@ def forgotpassword():
         message = msg.as_string()
         email.sendmail(fromaddr, toaddr, message)
         email.quit()
-        flash("Details verified. An email had been sent to you to reset the password.")
+        flash(u'Details verified. An email had been sent to you to reset the password','success')
         return render_template('index.html', message="User created successfully")
     else:
         abort(404)
@@ -324,6 +382,7 @@ def forgotpassword():
 @app.route('/signupload')
 def upload_signature_here():
     if 'user' in session:
+
         return render_template('signature_upload.html')
     return render_template('index.html',message="Not Logged in")
 
@@ -335,21 +394,48 @@ def allowed_file(filename):
 @app.route('/uploads')
 def upload_file():
     cursor1 = mysql.connection.cursor()
-    file_path = app.config['UPLOAD_FOLDER']+ "edge1.jpg"
+    file_path = session['file_path']
+    name=session['filename']
     print(file_path)
     username = session['user']
-    cursor1.execute("insert into Image_path (Name,Imagepath, username) values(%s,%s,%s)",["edge1.jpg", file_path, username])
+    print('beforeprefer')
+    print(request)
+    prename = request.form['preferred']
+    print('afterprefer')
+    cursor1.execute("select * from Image_path where username=%s",[username])
+    exists=cursor1.fetchall()
+    cursor1.execute("select Preferredname from Image_path where username=%s", [username])
+    preexists=cursor1.fetchall()
+    if not exists :
+        print('1111')
+        cursor1.execute("insert into Image_path (Name,Imagepath, username) values(%s,%s,%s)",[name, file_path, username])
+    elif not preexists:
+        cursor1.execute("update Image_path set Preferredname=%s where username=%s",[prename,username])
+    else:
+        print('1111')
+        cursor1.execute("update Image_path set Name=concat(Name,',',%s) where username=%s",[name,username])
+        cursor1.execute("update Image_path set Imagepath=concat(Imagepath,',',%s) where username=%s",[file_path,username])
+        cursor1.execute("update Image_path set Preferredname=concat(Preferredname,',',%s) where username=%s", [prename, username])
     mysql.connection.commit()
+    print('1111')
+    # code here
     return render_template('signature_upload.html',message="upload")
 
+
+
 @app.route('/uploads/<filename>',methods=['GET', 'POST'])
-def uploaded_file(filename):
-    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+def download_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename=filename, as_attachment=True)
 
 
 @app.route('/mlalgo', methods=['GET', 'POST'])
 def detect_signature():
+    cursor1 = mysql.connection.cursor()
     file_path = ''
+    username=session['user']
+
+
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -362,34 +448,39 @@ def detect_signature():
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
+
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
+            session['filename'] = filename
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             file_path = app.config['UPLOAD_FOLDER'] + filename
+            session['file_path']=file_path
 
-    img = cv2.imread(file_path)
-    b, g, r = cv2.split(img)  # get b,g,r
-    rgb_img = cv2.merge([r, g, b])  # switch it to rgb
+    print(file_path)
+   # img = cv2.imread(file_path)
+
+    #b, g, r = cv2.split(img)  # get b,g,r
+    #rgb_img = cv2.merge([r, g, b])  # switch it to rgb
 
     # Denoising
-    dst = cv2.fastNlMeansDenoising(img, None, 10, 7, 21)
+    #dst = cv2.fastNlMeansDenoising(img, None, 10, 7, 21)
 
-    b, g, r = cv2.split(dst)  # get b,g,r
-    rgb_dst = cv2.merge([r, g, b])  # switch it to rgb
+    #b, g, r = cv2.split(dst)  # get b,g,r
+    #rgb_dst = cv2.merge([r, g, b])  # switch it to rgb
 
-    cv2.imwrite(app.config['UPLOAD_FOLDER'] + "edge.jpg", rgb_dst)
+    #cv2.imwrite(app.config['UPLOAD_FOLDER'] + "edge.jpg", rgb_dst)
 
-    img = cv2.imread(app.config['UPLOAD_FOLDER'] + 'edge.jpg', 0)
-    edges = cv2.Canny(img, 80, 100)
+    #img = cv2.imread(app.config['UPLOAD_FOLDER'] + 'edge.jpg', 0)
+    #edges = cv2.Canny(img, 80, 100)
 
-    plt.subplot(121), plt.imshow(img, cmap='gray')
-    plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(edges, cmap='gray')
-    plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+    #plt.subplot(121), plt.imshow(img, cmap='gray')
+    #plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+    #plt.subplot(122), plt.imshow(edges, cmap='gray')
+    #plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
     # plt.show()
-    cv2.imwrite(app.config['UPLOAD_FOLDER'] + "edge1.jpg", edges)
-    return render_template('signature_upload.html', message ="success")
+    #cv2.imwrite(app.config['UPLOAD_FOLDER'] + "edge1.jpg", edges)
+    return render_template('signature_upload.html', message = "success")
 
 
 
